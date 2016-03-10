46056e61a928f21ce10dc7ff5c8dbab6e67b4758
Jakub Hladik <jhladik@iastate.edu>
2016-03-09 22:16:38 -0600

    Got it working - flashing LED

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <avr/io.h>
 
+#define F_CPU 250000UL
+#include <util/delay.h>
 
-int main(void)
+int main()
 {
-    // code
-    return 0;
+    DDRB = 0xFF;
+
+    while(1)
+    {
+        PORTB = 0xFF;
+        _delay_ms(250);
+        PORTB = 0x00;
+        _delay_ms(250);
+    }
+
 }
