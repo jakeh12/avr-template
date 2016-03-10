d05005ebe8a0685880376e006a44caf87661c594
Jakub Hladik <jhladik@iastate.edu>
2016-03-10 10:14:53 -0600

    Finally working

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <avr/io.h>
 
-#define F_CPU 250000UL
+#define F_CPU 16000000UL
 #include <util/delay.h>
+
+//  -U lfuse:w:0x6F:m // use transceiver oscillator
 
 int main()
 {
+
+    // set clock prescaler to 0
+    CLKPR = _BV(CLKPCE);
+    CLKPR = 0x00;
+
     DDRB = 0xFF;
 
     while(1)
