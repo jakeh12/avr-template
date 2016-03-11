f00ebfd8ffc2ade46f772ec8040a21c534325f5b
Jakub Hladik <jhladik@iastate.edu>
2016-03-11 11:59:11 -0600

    Display working

--- main.c
+++ main.c
@@ -63,7 +63,7 @@ void spi_init(void)
     DDRB |= _BV(DISP_D_C) | _BV(DISP_SCK) | _BV(DISP_MOSI);
     DDRD |= _BV(DISP_RST);
     DDRG |= _BV(DISP_SEL);
-    SPCR = _BV(SPE) | _BV(MSTR) | _BV(SPR1) | _BV(SPR0);
+    SPCR = _BV(SPE) | _BV(MSTR);
 }
 
 void spi_transmit(uint8_t data)
@@ -94,41 +94,108 @@ void display_reset(void)
     PORTD &= ~(_BV(DISP_RST));
     _delay_us (3);
     PORTD |= _BV(DISP_RST);
-    display_reset();
 }
 
 void display_init(void)
 {
     spi_init();
+
     display_reset();
+
     display_write_instruction(SSD1306_DISP_SLEEP);
-    display_write_instruction(SSD1306_SET_MULTIPLEX_RATIO);
-    display_write_instruction(0x3F);
-    display_write_instruction(SSD1306_SET_VERTICAL_OFFSET);
-    display_write_instruction(0x00);
-    display_write_instruction(SSD1306_SET_DISP_START_LINE);
-    display_write_instruction(SSD1306_SET_SEG_REMAP_0);
-    display_write_instruction(SSD1306_SET_COM_SCAN_NORMAL);
-    display_write_instruction(SSD1306_SET_WIRING_SCHEME);
-    display_write_instruction(0x12);
-    display_write_instruction(SSD1306_DEFAULT_CONTRAST);
-    display_write_instruction(SSD1306_RESUME_TO_RAM_CONTENT);
-    display_write_instruction(SSD1306_DISP_NORMAL);
-    display_write_instruction(SSD1306_MEM_ADDRESSING);
-    display_write_instruction(0x00);
+
     display_write_instruction(SSD1306_SET_DISP_CLOCK);
     display_write_instruction(0x80);
+
+    display_write_instruction(SSD1306_SET_MULTIPLEX_RATIO);
+    display_write_instruction(0x1F);
+
+    display_write_instruction(SSD1306_SET_VERTICAL_OFFSET);
+    display_write_instruction(0x00);
+
+    display_write_instruction(SSD1306_SET_DISP_START_LINE);
+
     display_write_instruction(SSD1306_CHARGE_PUMP_REGULATOR);
-    display_write_instruction(SSD1306_CHARGE_PUMP_ON);
+    display_write_instruction(0x14);
+
+    display_write_instruction(SSD1306_SET_SEG_REMAP_0);
+
+    display_write_instruction(SSD1306_SET_COM_SCAN_INVERTED);
+
+    display_write_instruction(SSD1306_SET_WIRING_SCHEME);
+    display_write_instruction(0x02);
+
+    display_write_instruction(SSD1306_SET_CONTRAST);
+    display_write_instruction(0x8F);
+
+    display_write_instruction(SSD1306_SET_PRECHARGE_PERIOD);
+    display_write_instruction(0xF1);
+
+    display_write_instruction(SSD1306_SET_VCOM_DESELECT_LEVEL);
+    display_write_instruction(0x40);
+
+    display_write_instruction(SSD1306_RESUME_TO_RAM_CONTENT);
+
+    display_write_instruction(SSD1306_DISP_NORMAL);
+
+    display_clear();
+
     display_write_instruction(SSD1306_DISP_ON);
 
 }
 
+void display_reset_cursor(void)
+{
+    display_write_instruction(SSD1306_SET_PAGE_START_ADDR);
+    display_write_instruction(SSD1306_SET_COL_HI_NIBBLE);
+    display_write_instruction(SSD1306_SET_COL_LO_NIBBLE);
+}
 
+void display_test(void)
+{
+    display_reset_cursor();
+    for (uint16_t byte = 0; byte < SSD1306_PIXEL_BYTES; byte++)
+    {
+        if (byte % 2 == 0)
+        {
+            display_write_data (0xFF);
+        }
+        else
+        {
+            display_write_data (0x00);
+        }
+    }
+}
+
+void display_clear(void)
+{
+    display_reset_cursor();
+    for (uint16_t byte = 0; byte < SSD1306_PIXEL_BYTES; byte++)
+    {
+            display_write_data (0x00);
+    }
+}
 
 int main()
 {
+    // set clock prescale to 0 -> 16 MHz
+    CLKPR = _BV(CLKPCE);
+    CLKPR = 0x00;
+
     display_init();
+
+    display_test();
+
+    DDRB |= _BV(PB4);
+
+    while(1)
+    {
+        PORTB |= _BV(PB4);
+        _delay_ms(100);
+        PORTB &= ~_BV(PB4);
+        _delay_ms(100);
+    }
+
 
 
 }
